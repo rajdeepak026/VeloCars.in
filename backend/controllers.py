cd532f187a45ac770da149b60fc5aa5b5a4c7ae0
Raj Deepak <rajdeepak02004@gmail.com>
2025-05-30 18:39:40 +0530

    login and signup system add

--- backend/controllers.py
+++ backend/controllers.py
@@ -2,14 +2,62 @@ from flask import Flask, render_template, redirect, request, session
 from flask import current_app as app 
 from .models import *
 
-@app.route("/index")
+@app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("landing-page.html")
 
-@app.route("/home")
-def home():
-    return render_template("home.html")
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        email = request.form.get("email")
+        password = request.form.get("password")
+        user = User.query.filter_by(email=email, password=password).first()
+        if user:
+            if user.password == password:
+                if user.type == "admin":
+                    return redirect("/admin")
+                else:
+                    return redirect(f"/user/{user.id}")
+            else:
+                return render_template("login.html", error="Invalid password")
+        else:
+            return render_template("login.html", error="User not found")
+    return render_template("login.html")
 
-@app.route("/services")
-def offer():
-    return render_template("services.html")
+@app.route("/signup", methods=["GET", "POST"])
+def signup():
+    if request.method == "POST":
+        fullname = request.form.get("fullname")
+        email = request.form.get("email")
+        mobile_no = request.form.get("mobile_no")
+        password = request.form.get("password")
+        confirm_password = request.form.get("confirm_password")
+
+        if password != confirm_password:
+            return render_template("signup.html", error="Passwords do not match")
+
+        this_user = User.query.filter_by(email=email).first()
+        if this_user:
+            return render_template("signup.html", error="User already exists")
+
+        new_user = User(fullname=fullname, email=email, password=password, mobile_no=mobile_no)
+        db.session.add(new_user)
+        db.session.commit()
+        return redirect("/login")
+
+    return render_template("signup.html")
+
+
+@app.route("/landing")
+def landing():
+    return render_template("landing-page.html")
+
+@app.route("/admin")
+def admin():
+    admin_user = User.query.filter_by(type="admin").first()
+    return render_template("admin-dash.html", admin_user=admin_user)
+
+@app.route("/user/<int:user_id>")
+def user_dash(user_id):
+    user = User.query.get(user_id)
+    return render_template("user-dash.html", user=user)
--- backend/models.py
+++ backend/models.py
@@ -1,1 +1,9 @@
 from .database import db
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    fullname = db.Column(db.String(100), nullable = False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    mobile_no = db.Column(db.String(15), nullable=False)
+    password = db.Column(db.String(100), nullable=False)
+    type = db.Column(db.String(10), nullable=False, default="user")  # "user" or "admin"
